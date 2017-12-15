aa498cfb7367cf1b09f14f8c11eb5468dc67b901
melpon <shigemasa7watanabe+github@gmail.com>
2017-12-15 22:43:49 +0900

    add prod environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApiUrl: 'https://wandbox.org/api/',
+  baseUrl: 'https://wandbox.org/',
+  DEFAULT_COMPILER: 'gcc-head'
 };
