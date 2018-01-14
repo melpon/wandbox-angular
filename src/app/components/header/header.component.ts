56cdf2097bb3c1e8dffcac0c157d4f93cc3d537d
melpon <shigemasa7watanabe+github@gmail.com>
2018-01-14 16:19:41 +0900

    Implement login to GitHub

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { LocalStorageService } from "../common/local-storage.service";
 
 @Component({
   selector: "wandbox-header",
@@ -6,7 +7,29 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent implements OnInit {
+  constructor(private storage: LocalStorageService) {
+  }
   public ngOnInit() {
+    // TODO(melpon): ログイン処理やヘッダーの表示に関しては、別コンポーネントにする
+    // ?session= なセッションがあれば、その値をローカルストレージに保存して、クエリ文字列を消しておく
+    if (window.location.search.length != 0) {
+      /* FIXME(melpon): URL は IE だと使えないらしい
+      const url = new URL(location.href);
+      url.searchParams.get("session");
+      */
+
+      const qs = window.location.search.slice(1);
+      let params: {[key: string]: string} = {};
+      for (const param of qs.split("&")) {
+        const xs = param.split("=");
+        params[xs[0]] = xs[1];
+      }
+      if (params["session"] != null) {
+        this.storage.setValue("session", params["session"]);
+      }
+      window.history.replaceState(null, null, window.location.pathname);
+    }
+
     return undefined;
   }
 }
